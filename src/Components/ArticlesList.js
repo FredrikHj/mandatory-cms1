cefb3db59d505a78c929bcfe0640ee4acf375dc0
Your Name <you@example.com>
2019-09-03 15:33:54 +0200

    Klar 80%

--- src/Components/ArticlesList.js
+++ src/Components/ArticlesList.js
@@ -4,13 +4,14 @@ import axios from 'axios';
 
 export let ArticlesList = (props) => {
   let [ incomminArticles, setIncomminArticles ] = useState([]);
-/*   let showList = props.sendArticlesList;
- */
+  let [ pagSkip, setPagSkip ] = useState(10);
+  let [ pagLimit, setPagLimit ] = useState(1);
+
   useEffect(() =>{
     console.log();
     
     // Get Articles
-    axios.get('http://192.168.99.100:8080/api/collections/get/Artiklar', {
+    axios.get('http://192.168.99.100:8080/api/collections/get/Artiklar?skip=' + pagSkip + '&limit=' + pagLimit, {
         headers: { 'Cockpit-Token': '3dcadbb31033dd704673a595544b15}' }
     })
     .then(response => {
@@ -21,15 +22,34 @@ export let ArticlesList = (props) => {
         console.log(error);
     });
   }, []);
-
+  function searchArticle(e) {
+    let targetArticle = e.target.value;
+    console.log(targetArticle);
+    
+  }
+  function changeShowPage(e) {
+    let targetNr = e.target.value;
+    setPagSkip(targetNr);
+    console.log(targetNr);
+    
+  }
+  function changeShowEach(e) {
+    let targetNr = e.target.value;
+    setPagLimit(targetNr);
+    console.log(targetNr);
+  }
   return(
     <div className="page">
       <p className="headLine">Författarblogg</p>
-      Sök efter en film:<br/>
-      <input type="text" />
-      Visa
-      Sidor --> <input type="number"/>
-      Antal --> <input type="number" />
+      
+      Sök en artikel --> <input className="inputWitdhSearch" type="text" onChange={ searchArticle }/><br/><br/>
+      <section id="pagContainer">
+        Visa endast<br/>
+        <section id="articlesPagination">
+          <p> Antal Sidor --> <input className="inputWitdh" type="number" onChange={ changeShowPage }/></p> 
+          <p> Antal artiklar --> <input  className="inputWitdh" type="number" onChange={ changeShowEach }/></p> 
+        </section>
+      </section>
       <table id="articles">
         <thead>
         <tr><th>Titel</th><th>Författare</th><th>Datum</th></tr>
